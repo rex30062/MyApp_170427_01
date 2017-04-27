cea18af7d246cd4a3af9d0c8a7806108560698df
Cclz <rex620620>
2017-04-27 15:08:24 +0800

    Firebase Auth

--- app/src/main/java/com/cclz/myapp_170427_01/MainActivity.java
+++ app/src/main/java/com/cclz/myapp_170427_01/MainActivity.java
@@ -1,12 +1,18 @@
 package com.cclz.myapp_170427_01;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,13 +23,16 @@ public class MainActivity extends AppCompatActivity {
     // Write a message to the database
     FirebaseDatabase database;
     DatabaseReference myRef;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference("message");
+        mAuth = FirebaseAuth.getInstance();
         Intent it=getIntent();
+
         String msg=it.getStringExtra("msg");
         if(msg != null){
             TextView tv=(TextView) findViewById(R.id.textView);
@@ -48,5 +57,19 @@ public class MainActivity extends AppCompatActivity {
 
         EditText ed=(EditText) findViewById(R.id.editText);
         myRef.setValue(ed.getText().toString());
+    }
+
+    public void clickLogin(View v){
+        mAuth.signInWithEmailAndPassword("rex62062002@yahoo.com.tw", "12345678").addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "登入成功", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "登入失敗", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
